afd5b311be9734160a9e0693d0f82089afb64c33
ShounakRoy23062 <shounakofficial02@gmail.com>
2026-05-05 16:34:48 +0800

    UC13: Centralized Arithmetic Logic to Enforce DRY in Quantity Operations

--- APP/src/QuantityMeasurementApp.java
+++ APP/src/QuantityMeasurementApp.java
@@ -1,23 +1,7 @@
 public class QuantityMeasurementApp {
 
-    public static <U extends IMeasurable> void demonstrateEquality(Quantity<U> q1, Quantity<U> q2) {
-        System.out.println(q1.equals(q2));
-    }
-
-    public static <U extends IMeasurable> void demonstrateConversion(Quantity<U> q, U target) {
-        System.out.println(q.convertTo(target));
-    }
-
-    public static <U extends IMeasurable> void demonstrateAddition(Quantity<U> q1, Quantity<U> q2, U target) {
-        System.out.println(q1.add(q2, target));
-    }
-
-    public static <U extends IMeasurable> void demonstrateSubtraction(Quantity<U> q1, Quantity<U> q2, U target) {
-        System.out.println(q1.subtract(q2, target));
-    }
-
-    public static <U extends IMeasurable> void demonstrateDivision(Quantity<U> q1, Quantity<U> q2) {
-        System.out.println(q1.divide(q2));
+    public static <U extends IMeasurable> void show(String msg, Object val) {
+        System.out.println(msg + " : " + val);
     }
 
     public static void main(String[] args) {
@@ -25,19 +9,22 @@ public class QuantityMeasurementApp {
         Quantity<LengthUnit> l1 = new Quantity<>(10.0, LengthUnit.FEET);
         Quantity<LengthUnit> l2 = new Quantity<>(6.0, LengthUnit.INCHES);
 
-        demonstrateSubtraction(l1, l2, LengthUnit.FEET);
-        demonstrateDivision(l1, new Quantity<>(2.0, LengthUnit.FEET));
+        show("Length Add", l1.add(l2));
+        show("Length Sub", l1.subtract(l2));
+        show("Length Div", l1.divide(new Quantity<>(2.0, LengthUnit.FEET)));
 
         Quantity<WeightUnit> w1 = new Quantity<>(10.0, WeightUnit.KILOGRAM);
         Quantity<WeightUnit> w2 = new Quantity<>(5000.0, WeightUnit.GRAM);
 
-        demonstrateSubtraction(w1, w2, WeightUnit.KILOGRAM);
-        demonstrateDivision(w1, new Quantity<>(5.0, WeightUnit.KILOGRAM));
+        show("Weight Add", w1.add(w2));
+        show("Weight Sub", w1.subtract(w2));
+        show("Weight Div", w1.divide(new Quantity<>(5.0, WeightUnit.KILOGRAM)));
 
         Quantity<VolumeUnit> v1 = new Quantity<>(5.0, VolumeUnit.LITRE);
         Quantity<VolumeUnit> v2 = new Quantity<>(500.0, VolumeUnit.MILLILITRE);
 
-        demonstrateSubtraction(v1, v2, VolumeUnit.LITRE);
-        demonstrateDivision(v1, new Quantity<>(10.0, VolumeUnit.LITRE));
+        show("Volume Add", v1.add(v2));
+        show("Volume Sub", v1.subtract(v2));
+        show("Volume Div", v1.divide(new Quantity<>(10.0, VolumeUnit.LITRE)));
     }
 }
